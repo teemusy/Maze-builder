a27de5f757f7e5a0c317a2ac1fe74f77c8be3b1c
unknown <teemu.syvaoja@gmail.com>
2018-03-22 11:25:12 +0200

    Reworking on game logic

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import cairo
 import sys
 print(sys.version)
 
+#TODO
+#add library for directions
+
 #CONSTANTS
 ROWS = 6
 COLUMNS = 6
 LOOPS = ROWS*COLUMNS
-maze = []
 WIDTH, HEIGHT = 512, 512
 START_X, START_Y = WIDTH/2, 0
 CELL_SIZE = 1.0/ROWS
@@ -20,6 +22,9 @@ DEBUG_MODE = 1
 #north, east, south, west, visited
 cell = [1,1,1,1,0]
 cell_list = []
+maze = []
+#dictionary for directions, not working
+dir = {"NORTH": "[grid_row-1][grid_column]", "SOUTH": "grid_row+1"}
 
 #FUNCTION DECLARATIONS
 
@@ -43,93 +48,116 @@ def fill_map (list):
 
 #uses growing tree algorithm to build the maze
 #http://weblog.jamisbuck.org/2011/1/27/maze-generation-growing-tree-algorithm
-def build_maze_old (c_list, maze_list):
+def build_maze (c_list, maze_list):
 
-	for xyz in range (0,LOOPS):
+	for xyz in range (0,LOOPS*100):
+		#choose random cell from c_list
+		#choose random direction
+			#check if it's unvisited
+			#if so carve path there and add location to c_list
+			#if not check next direction until all directions have been checked
+		#remove visited cell from list
+		#repeat
 		cell_list_size = len(c_list)
+		#break loop if cell list size == 0
+		if cell_list_size == 0:
+			break
+		
 		#choose random cell where to start
-		random_cell = random_number_generator(0, cell_list_size-1)
+		random_cell = 0
+		if cell_list_size > 0:
+			random_cell = random_number_generator(0, cell_list_size-1)
+		
 		grid_row = c_list[random_cell][0]
 		grid_column = c_list [random_cell][1]
 		
 		#convert list to int
 		grid_row = grid_row[0]
 		grid_column = grid_column[0]
-
-		
-		directions = 0
-		#choose direction, change to random later
-		direction_to_carve = 0
-		#no switch case in python!!11 :(
-		#check all directions first?
-		#north
-		while directions < 4:
-			directions += 1
-			
+		n = e = s = w = 0
+		found_legal_direction = 0
+		#loop until all directions checked or found legal direction
+		while (n == 0 or e == 0 or s == 0 or w == 0) and found_legal_direction == 0:
+			direction_to_carve = random_number_generator (0, 3)
 			if direction_to_carve == 0:
-				#check that list index is not out of range
+				n = 1		
+			elif direction_to_carve == 1:
+				e = 1		
+			elif direction_to_carve == 2:
+				s = 1		
+			elif direction_to_carve == 3:
+				w = 1
+			
+			
+			#north
+			if direction_to_carve == 0:
+				
+				#check if north is legal direction and cell in north has not been visited
 				if grid_row > 0:
-					if maze_list [grid_row-1][grid_column][4] == 0:
-						#add new cell to c_list
+					if maze_list[grid_row-1][grid_column][4] == 0:
+						#add direction to c_list
 						sub = [grid_row-1],[grid_column]
 						c_list.append(sub)
-						#carve walls from both cells
-						maze_list [grid_row][grid_column][0] = 0
-						maze_list [grid_row-1][grid_column][3] = 0
-						#mark as visited
-						maze_list [grid_row][grid_column][4] = 1
-						maze_list [grid_row-1][grid_column][4] = 1
-						direction_to_carve += 1
+						#add visited flag
+						maze_list[grid_row-1][grid_column][4] = 1
+						#carve wall
+						maze_list[grid_row][grid_column][0] = 0
+						maze_list[grid_row-1][grid_column][2] = 0
+						found_legal_direction = 1			
+			
 			#east
-			elif direction_to_carve == 1:
+			if direction_to_carve == 1:
+				
+				#check if east is legal direction and cell in east has not been visited
 				if grid_column < COLUMNS-1:
-					if maze_list [grid_row][grid_column+1][4] == 0:
-						#add new cell to c_list
-						sub = [grid_row],[grid_column+1]
-						c_list.append(sub)
-						#carve walls from both cells
-						maze_list [grid_row][grid_column][1] = 0
-						maze_list [grid_row-1][grid_column+1][4] = 0
-						#mark as visited
-						maze_list [grid_row][grid_column][4] = 1
-						maze_list [grid_row-1][grid_column+1][4] = 1
-						direction_to_carve += 1
+						if maze_list[grid_row][grid_column+1][4] == 0:
+							#add direction to c_list
+							sub = [grid_row],[grid_column+1]
+							c_list.append(sub)
+							#add visited flag
+							maze_list[grid_row][grid_column+1][4] = 1
+							#carve wall
+							maze_list[grid_row][grid_column][1] = 0
+							maze_list[grid_row][grid_column+1][3] = 0
+							found_legal_direction = 1			
+					
 			#south
-			elif direction_to_carve == 2:
+			if direction_to_carve == 2:
+				
+				#check if south is legal direction and cell in south has not been visited
 				if grid_row < ROWS-1:
-					if maze_list [grid_row+1][grid_column][4] == 0:
-						#add new cell to c_list
+					if maze_list[grid_row+1][grid_column][4] == 0:
+						#add direction to c_list
 						sub = [grid_row+1],[grid_column]
 						c_list.append(sub)
-						#carve walls from both cells
-						maze_list [grid_row][grid_column][3] = 0
-						maze_list [grid_row+1][grid_column][0] = 0
-						#mark as visited
-						maze_list [grid_row][grid_column][4] = 1
-						maze_list [grid_row+1][grid_column][4] = 1
-						direction_to_carve += 1
+						#add visited flag
+						maze_list[grid_row+1][grid_column][4] = 1
+						#carve wall
+						maze_list[grid_row][grid_column][2] = 0
+						maze_list[grid_row-1][grid_column][0] = 0
+						found_legal_direction = 1			
+					
 			#west
-			elif direction_to_carve == 3:
-				if grid_column > 0:
-					if maze_list [grid_row][grid_column-1][4] == 0:
-						#add new cell to c_list
-						sub = [grid_row-1],[grid_column-1]
-						c_list.append(sub)
-						#carve walls from both cells
-						maze_list [grid_row][grid_column][4] = 0
-						maze_list [grid_row-1][grid_column-1][1] = 0
-						#mark as visited
-						maze_list [grid_row][grid_column][4] = 1
-						maze_list [grid_row-1][grid_column-1][4] = 1
-						direction_to_carve += 1
-			else:
-				#remove random_cell from clist
-				c_list.remove(random_cell)
-			directions += 1
-
+			if direction_to_carve == 3:
+				
+				#check if west is legal direction and cell in west has not been visited
+				if grid_column > 0 and maze_list[grid_row][grid_column-1][4] == 0:
+					#add direction to c_list
+					sub = [grid_row],[grid_column-1]
+					c_list.append(sub)
+					#add visited flag
+					maze_list[grid_row][grid_column-1][4] = 1
+					#carve wall
+					maze_list[grid_row][grid_column][3] = 0
+					maze_list[grid_row][grid_column-1][1] = 0
+					found_legal_direction = 1
+		
+		if n == 1 and e == 1 and s == 1 and w == 1:
+	
+			c_list.pop(random_cell)
 		
 		
-def build_maze (c_list, maze_list):		
+def build_maze_old (c_list, maze_list):		
 	for xyz in range (0,LOOPS*1000):
 		#choose random cell from c_list
 		#choose random direction
